866d33357d257ca6c457030ad6d2d5b595f96a0e
Nikola Spasic <nspasic96@gmail.com>
2016-04-25 01:49:22 +0200

    students page, some js added

---
+++ js/studenti.js
@@ -0,0 +1,8 @@
+function onMouseOverZoom(val) {
+	val.style.width = (100).toString()+"px";
+	val.style.height = (100).toString()+"px";
+}
+function onMouseOutUnzoom(val) {
+	val.style.width = (50).toString()+"px";
+	val.style.height = (50).toString()+"px";
+}
