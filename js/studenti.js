99a7c5d15a473b5c6536f1b28e448b82d2ff259b
Nikola Spasic <nspasic96@gmail.com>
2016-05-07 19:35:54 +0200

    background changed and some scripts added

--- js/studenti.js
+++ js/studenti.js
@@ -1,8 +1,33 @@
-function onMouseOverZoom(val) {
-	val.style.width = (100).toString()+"px";
-	val.style.height = (100).toString()+"px";
+var nizStudenata = [
+	{
+		id:11,
+		brPoena:0,
+		srcSlike: "../../../stranice/11",
+		srcStranice: "../../../showimage.php?id=3546",
+		klasa: "studentslika",
+		fullName: "Nikola Pavlović"
+	},
+	{
+		id:22,
+		brPoena:0,
+		srcSlike: "../../../stranice/22",
+		srcStranice: "../../../showimage.php?id=3557",
+		klasa: "studentslika",
+		fullName: "Saša Đorđević"
+	}
+];
+for(var i=35; i<=88; i++){
+	var student={
+		id:i,
+		brPoena:0,
+		srcSlike: "../../../stranice/"+String(i),
+		srcStranice: "../../../showimage.php?id=" + String(i+3535),
+		klasa: "studentslika",
+		fullName: ""
+	};
+	student.fullName="Treba izvući puno ime sa stranice rezultati!";
+	nizStudenata.push(student);
 }
-function onMouseOutUnzoom(val) {
-	val.style.width = (50).toString()+"px";
-	val.style.height = (50).toString()+"px";
-}
+
+
+
